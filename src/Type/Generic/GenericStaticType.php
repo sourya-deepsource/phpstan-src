c6c5bf658f4a611035c15838267d974911abfcdc
Ondrej Mirtes <ondrej@mirtes.cz>
2025-02-01 15:58:16 +0100

    Fix

--- src/Type/Generic/GenericStaticType.php
+++ src/Type/Generic/GenericStaticType.php
@@ -82,7 +82,7 @@ class GenericStaticType extends StaticType
 		return $this->staticObjectType;
 	}
 
-	public function changeBaseClass(ClassReflection $classReflection): self
+	public function changeBaseClass(ClassReflection $classReflection): StaticType
 	{
 		if ($classReflection->getName() === $this->getClassName()) {
 			return $this;
